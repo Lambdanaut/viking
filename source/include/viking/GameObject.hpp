e7756a70f0aa5e40ff40b6812d7c407adca785f2
Nicolas Guillemot <nlguillemot@gmail.com>
2012-06-03 21:41:24 -0700

    updated todo

--- source/include/viking/GameObject.hpp
+++ source/include/viking/GameObject.hpp
@@ -8,16 +8,13 @@
 namespace vik
 {
 
-/*
-Base class for all GameObjects in the game.
-GameObjects have a state that is updated as a function of time with update()
-GameObjects are possible to serialize/deserialize. (Inherited from Serializable)
-GameObjects have their allocation managed by reference counting.
-TODO: Record a list of all GameObject allocations and deallocations and print some information to detect memory leaks in debug builds.
-TODO: Either that, or overload the global new operator (though I find this technique to be an anti-pattern for many reasons)
-
-On inheritance: Try to only subclass this class once. Twice is the absolute maximum, and multiple inheritance of this class is not allowed.
-*/
+// Base class for all GameObjects in the game.
+// GameObjects have a state that is updated as a function of time with update()
+// GameObjects are possible to serialize/deserialize. (Inherited from Serializable)
+// GameObjects have their allocation managed by reference counting.
+// TODO: Record a list of all GameObject allocations and deallocations and print some information to detect memory leaks in debug builds.
+// TODO: Either that, or overload the global new operator (though I find this technique to be an anti-pattern for many reasons)
+// On inheritance: Try to only subclass this class once. Twice is the absolute maximum, and multiple inheritance of this class is not allowed.
 class GameObject : public Serializable, public ReferenceCounted
 {
 public:
