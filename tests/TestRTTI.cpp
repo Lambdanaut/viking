23406c8d1ace496844bd0e9fa7686e8f25e2d328
Nicolas Guillemot <nlguillemot@gmail.com>
2012-06-07 09:49:58 -0700

    added getBase function and fixed bug by making definitions static

--- tests/TestRTTI.cpp
+++ tests/TestRTTI.cpp
@@ -2,8 +2,13 @@
 #include <iostream>
 #include <cassert>
 #include <map>
-
+#include <cstring>
 #include "viking/HashedString.hpp"
+
+// future ideas: 
+// constructing classes from Class objects
+// also reflecting available member functions in one way or other
+// hooking up to scripting engine
 
 namespace vik
 {
@@ -24,22 +29,43 @@ public:
 		return className;
 	}
 
-	bool instanceOf(const Class& r) const
+	// returns true if the Class is a parent/derivation/subclass of the other
+	bool derivesFrom(const Class& other) const
 	{
-		if ( this == &r )
+		if (this == &other)
 		{
 			return true;
 		}
 
-		for ( unsigned i = 0; i < parents.size(); ++i )
+		for (unsigned i = 0; i < parents.size(); ++i)
 		{
-			if ( parents[i]->instanceOf( r ) )
+			if (parents[i]->derivesFrom(other))
 			{
 				return true;
 			}
 		}
 
 		return false;
+	}
+
+	// returns ancestor/base/super Class by name. Returns 0 if the parent is not found.
+	// will return "this" also if you ask for the same class as what it is.
+	const Class* getBase(const char* baseName) const
+	{
+		if (strcmp(className, baseName) == 0)
+		{
+			return this;
+		}
+
+		for (unsigned i = 0; i < parents.size(); ++i)
+		{
+			if (const Class* myParent = parents[i]->getBase(baseName))
+			{
+				return myParent;
+			}
+		}
+
+		return 0;
 	}
 private:
 	const char* className;
@@ -62,7 +88,7 @@ public:
 	// add new class definition and return it
 	// if class already exists, will return the already existing instance of it.
 	template<class Derived, class... Parents>
-	static const Class* addClass(const char* className)
+	const Class* addClass(const char* className)
 	{
 		if (const Class* found = findClass(className))
 		{
@@ -70,48 +96,50 @@ public:
 		}
 
 		Class* newClass = new Class(className, { &Parents::getClass()... });
-		getSingleton().classTable[HashedString(className)] = newClass;
+		classTable[HashedString(className)] = newClass;
 
 		return newClass;
 	}
 
 	// lookup class definition from hashed name
-	static const Class* findClass(const HashedString& hash)
+	const Class* findClass(const HashedString& hash)
 	{
-		std::map<HashedString, Class*>::const_iterator it = getSingleton().classTable.find(hash);
+		std::map<HashedString, Class*>::const_iterator it = classTable.find(hash);
 
-		if (it == getSingleton().classTable.end())
+		if (it == classTable.end())
 		{
 			return 0;
 		}
 
-		return getSingleton().classTable[hash];
+		return classTable[hash];
 	}
 
 	// lookup class definition from name
-	static const Class* findClass(const char* className)
+	const Class* findClass(const char* className)
 	{
 		return findClass(HashedString(className));
 	}
 
 	// for debugging the RTTI system
-	static void debugPrint()
+	// todo: show hierarchy as well
+	void debugPrint()
 	{
 		std::cout << "address : class name" << std::endl;
 
-		const std::map<HashedString, Class*>::const_iterator end = getSingleton().classTable.end();
-		for (std::map<HashedString, Class*>::iterator it = getSingleton().classTable.begin(); it != end; ++it)
+		const std::map<HashedString, Class*>::const_iterator end = classTable.end();
+		for (std::map<HashedString, Class*>::iterator it = classTable.begin(); it != end; ++it)
 		{
 			std::cout << (*it).second << " : " << std::flush;
 			std::cout << (*it).second->getName() << std::endl;
 		}
 	}
-private:
+
 	static Reflection& getSingleton()
 	{
 		static Reflection refl;
 		return refl;
 	}
+private:
 
 	Reflection() = default;
 
@@ -120,54 +148,74 @@ private:
 };
 
 // Declares necessary members for RTTI. To be used within the class declaration.
-// Watch out: ClassInfo(SomeClass::Nested) creates a different definition than ClassInfo(Nested) but both are accepted.
-#define ClassInfo(ThisClass, Parents...) \
+// the _classInfo variable is an implementation detail. Use ::getClass() to access it instead.
+#define ClassDeclare() \
+	static const Class* _classInfo; \
 	virtual const Class& getInstanceClass() const \
 	{ \
-		return ThisClass::getClass(); \
+		return *_classInfo; \
 	} \
 	static const Class& getClass() \
 	{ \
-		static const Class& r = *Reflection::addClass<ThisClass,##Parents>(#ThisClass); \
-		return r; \
+		return *_classInfo; \
 	} 
+
+// Defines a new Class object with the given parents and adds it to the Reflection singleton at static initialization time.
+// Watch out: ClassDefine(SomeClass::NestedClass) creates a different definition than ClassInfo(NestedClass) but both are accepted.
+#define ClassDefine(ThisClass, Parents...) \
+const Class* ThisClass::_classInfo = Reflection::getSingleton().addClass<ThisClass,##Parents>(#ThisClass);
 
 } // end namespace vik
 
 using namespace vik;
 
 // Unit tests
+
+class StaffMember
+{
+public:
+	ClassDeclare();
+};
+
+class Librarian : virtual public StaffMember
+{
+public:
+	ClassDeclare();
+};
+
+class Teacher : virtual public StaffMember
+{
+public:
+	ClassDeclare();
+};
+
+class TeachingLibrarian : public Teacher, public Librarian
+{
+public:
+	ClassDeclare();
+};
+
+class Sailboat
+{
+public:
+	ClassDeclare();
+};
+
+class Dog
+{
+public:
+	ClassDeclare();
+};
+
+ClassDefine(StaffMember);
+ClassDefine(Librarian,StaffMember);
+ClassDefine(Teacher,StaffMember);
+ClassDefine(TeachingLibrarian,Teacher,Librarian);
+ClassDefine(Sailboat);
+ClassDefine(Dog);
+
 class UnitTest
 {
-	class StaffMember
-	{
-	public:
-		ClassInfo(StaffMember);
-	};
-
-	class Librarian : virtual public StaffMember
-	{
-	public:
-		ClassInfo(Librarian,StaffMember);
-	};
-
-	class Teacher : virtual public StaffMember
-	{
-	public:
-		ClassInfo(Teacher,StaffMember);
-	};
-
-	class TeachingLibrarian : public Teacher, public Librarian
-	{
-	public:
-		ClassInfo(TeachingLibrarian,Teacher,Librarian);
-	};
-
-	class Sailboat
-	{
-	public:
-		ClassInfo(Sailboat);
-	};
 
 	void classlessRTTITest()
 	{
@@ -181,22 +229,31 @@ class UnitTest
 		assert(std::string(vehicleType.getName()) == "Vehicle");
 
 		// single inheritance valid upcast
-		assert(landVehicleType.instanceOf(vehicleType));
+		assert(landVehicleType.derivesFrom(vehicleType));
 
 		// single inheritance invalid upcast
-		assert(landVehicleType.instanceOf(fruitType) == false);
+		assert(landVehicleType.derivesFrom(fruitType) == false);
 
 		// single inheritance invalid cross-cast
-		assert(landVehicleType.instanceOf(waterVehicleType) == false);
+		assert(landVehicleType.derivesFrom(waterVehicleType) == false);
 
 		// multiple inheritance 1 level valid upcast
-		assert(amphibiousVehicleType.instanceOf(landVehicleType));
+		assert(amphibiousVehicleType.derivesFrom(landVehicleType));
 
 		// multiple inheritance 2 level valid upcast
-		assert(amphibiousVehicleType.instanceOf(vehicleType));
+		assert(amphibiousVehicleType.derivesFrom(vehicleType));
 
 		// multiple inheritance 1 level invalid upcast
-		assert(amphibiousVehicleType.instanceOf(fruitType) == false);
+		assert(amphibiousVehicleType.derivesFrom(fruitType) == false);
+
+		// getting valid grandparent class by name
+		assert(amphibiousVehicleType.getBase("Vehicle") == &vehicleType);
+
+		// getting valid parent class by name
+		assert(amphibiousVehicleType.getBase("WaterVehicle") == &waterVehicleType);
+
+		// getting valid self class by name
+		assert(waterVehicleType.getBase("WaterVehicle") == &waterVehicleType);
 
 		std::cout << "Classless tests successful" << std::endl;
 	}
@@ -216,25 +273,25 @@ class UnitTest
 		assert(std::string(staff->getInstanceClass().getName()) == "StaffMember");
 
 		// single inheritance valid upcast
-		assert(librarian->getInstanceClass().instanceOf(staff->getInstanceClass()));
+		assert(librarian->getInstanceClass().derivesFrom(staff->getInstanceClass()));
 
 		// single inheritance valid upcast through static member
-		assert(librarian->getInstanceClass().instanceOf(StaffMember::getClass()));
+		assert(librarian->getInstanceClass().derivesFrom(StaffMember::getClass()));
 
 		// single inheritance invalid upcast
-		assert(librarian->getInstanceClass().instanceOf(sailboat->getInstanceClass()) == false);
+		assert(librarian->getInstanceClass().derivesFrom(sailboat->getInstanceClass()) == false);
 
 		// single inheritance invalid cross-cast
-		assert(librarian->getInstanceClass().instanceOf(teacher->getInstanceClass()) == false);
+		assert(librarian->getInstanceClass().derivesFrom(teacher->getInstanceClass()) == false);
 
 		// multiple inheritance 1 level valid upcast
-		assert(teachingLibrarian->getInstanceClass().instanceOf(librarian->getInstanceClass()));
+		assert(teachingLibrarian->getInstanceClass().derivesFrom(librarian->getInstanceClass()));
 
 		// multiple inheritance 2 level valid upcast
-		assert(teachingLibrarian->getInstanceClass().instanceOf(staff->getInstanceClass()));
+		assert(teachingLibrarian->getInstanceClass().derivesFrom(staff->getInstanceClass()));
 
 		// multiple inheritance 1 level invalid upcast
-		assert(teachingLibrarian->getInstanceClass().instanceOf(sailboat->getInstanceClass()) == false);
+		assert(teachingLibrarian->getInstanceClass().derivesFrom(sailboat->getInstanceClass()) == false);
 
 		delete staff;
 		delete librarian;
@@ -249,20 +306,23 @@ class UnitTest
 	void reflectionTest()
 	{
 		// getting existing class by name
-		assert(Reflection::findClass("Sailboat") == &Sailboat::getClass());
+		assert(Reflection::getSingleton().findClass("Sailboat") == &Sailboat::getClass());
 
 		// getting nonexistant class by name
-		assert(Reflection::findClass("ASFJasdf") == 0);
+		assert(Reflection::getSingleton().findClass("ASFJasdf") == 0);
 
 		// getting existing class by hashed name
-		assert(Reflection::findClass(HashedString("Sailboat")) == &Sailboat::getClass());
+		assert(Reflection::getSingleton().findClass(HashedString("Sailboat")) == &Sailboat::getClass());
 
 		// getting nonexistant class by hashed name
-		assert(Reflection::findClass(HashedString("ASFJasdf")) == 0);
+		assert(Reflection::getSingleton().findClass(HashedString("ASFJasdf")) == 0);
+
+		// getting class that exists but was not used previously
+		assert(Reflection::getSingleton().findClass("Dog") != 0);
 
 		// print all classes for sanity check
 		std::cout << "reflection debug information:" << std::endl;
-		Reflection::debugPrint();
+		Reflection::getSingleton().debugPrint();
 
 		std::cout << "Reflection tests successful" << std::endl;
 	}
